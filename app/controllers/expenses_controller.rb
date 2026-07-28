7cc201633fec9c9bfc297279b7b8da7d173b04e0
PauloLondini <82684421+PauloLondini@users.noreply.github.com>
2026-07-28 19:56:37 +0000

    Reconstrucción del CRUD de gastos

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,51 @@
 class ExpensesController < ApplicationController
+
+  def index
+    if params[:category].present?
+      expenses = Expense.where(category: params[:category])
+    else
+      expenses = Expense.all
+    end
+
+    @total = expenses.sum(:amount)
+
+    @expenses = expenses.group_by { |expense| expense.category }
+  end
+
+  def create
+    Expense.create(
+      title: params[:title],
+      amount: params[:amount],
+      category: params[:category],
+      date: params[:date]
+    )
+
+    redirect_to "/expenses"
+  end
+
+  def edit
+    @expense = Expense.find(params[:id])
+  end
+
+  def update
+    @expense = Expense.find(params[:id])
+
+    @expense.update(
+      title: params[:title],
+      amount: params[:amount],
+      category: params[:category],
+      date: params[:date]
+    )
+
+    redirect_to "/expenses"
+  end
+
+  def destroy
+    @expense = Expense.find(params[:id])
+
+    @expense.destroy
+
+    redirect_to "/expenses"
+  end
+
 end
